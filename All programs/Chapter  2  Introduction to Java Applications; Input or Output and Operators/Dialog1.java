1f0c72223037bb3f4f993e885b77d0000f8d58d3
Yifan Zhu <zhuyi@tcd.ie>
2019-10-26 19:56:48 +0100

    Create Dialog1.java

---
+++ All programs/Chapter  2  Introduction to Java Applications; Input or Output and Operators/Dialog1.java
@@ -0,0 +1,10 @@
+// Fig. 2.18: Dialog1.java
+// Using JOptionPane to display multiple lines in a dialog box.
+import javax.swing.JOptionPane;
+
+public class Dialog1 {
+	public static void main(String[] args) {
+		// display a dialog with a message
+		JOptionPane.showMessageDialog(null, "Welcome to Java");
+	}
+} // end class Dialog1
