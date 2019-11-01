92dd36e0e85f1f1b4a26a8668128ef519dc7ed70
Yifan Zhu <zhuyi@tcd.ie>
2019-11-01 21:14:27 +0000

    Create DoWhileTest.java

---
+++ All programs/Chapter  4  Control Statements: Part 2; Logical Operators/DoWhileTest.java
@@ -0,0 +1,15 @@
+// Fig. 4.7: DoWhileTest.java
+// do...while repetition statement.
+
+public class DoWhileTest {
+	public static void main(String[] args) {
+		int counter = 1;
+		
+		do {
+			System.out.printf("%d  ", counter);
+			++counter;
+		} while (counter <= 10); // end do...while
+		
+		System.out.println();
+	}
+} // end class DoWhileTest
