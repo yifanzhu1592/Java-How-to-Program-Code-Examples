3013a3652006bfa282f069164e212a47fc045602
Yifan Zhu <zhuyi@tcd.ie>
2019-11-01 17:47:05 +0000

    Create Sum.java

---
+++ All programs/Chapter  4  Control Statements: Part 2; Logical Operators/Sum.java
@@ -0,0 +1,15 @@
+// Fig. 4.5: Sum.java
+// Summing integers with the for statement.
+
+public class Sum {
+	public static void main(String[] args) {
+		int total = 0;
+		
+		// total even integers from 2 through 20
+		for (int number = 2; number <= 20; number += 2) {
+			total += number;
+		}
+		
+		System.out.printf("Sum is %d%n", total);
+	}
+} // end class Sum
