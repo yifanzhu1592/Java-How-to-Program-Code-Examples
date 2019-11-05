e793c82488ebb69f03afb22321fc01e65232bac1
Yifan Zhu <zhuyi@tcd.ie>
2019-11-05 08:45:07 +0000

    Create DrawSmiley.java

---
+++ All programs/Chapter  5  Methods/DrawSmiley.java
@@ -0,0 +1,40 @@
+// Fig. 5.11: DrawSmiley.java
+// Drawing a smiley face using colors and filled shapes.
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class DrawSmiley extends JPanel {
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		// draw the face
+		g.setColor(Color.YELLOW);
+		g.fillOval(10, 10, 200, 200);
+		
+		// draw the eyes
+		g.setColor(Color.BLACK);
+		g.fillOval(55, 65, 30, 30);
+		g.fillOval(135, 65, 30, 30);
+		
+		// draw the mouth
+		g.fillOval(50, 110, 120, 60);
+		
+		// "touch up" the mouth into a smile
+		g.setColor(Color.YELLOW);
+		g.fillRect(50, 110, 120, 30);
+		g.fillOval(50, 120, 120, 40);
+	}
+	
+	public static void main(String[] args) {
+		DrawSmiley panel = new DrawSmiley();
+		JFrame application = new JFrame();
+		
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		application.add(panel);
+		application.setSize(230, 250);
+		application.setVisible(true);
+	}
+} // end class DrawSmiley
