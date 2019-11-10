a77a9da57d2bdb1124982309604b5a1da553edc5
Yifan Zhu <zhuyi@tcd.ie>
2019-11-10 13:37:25 +0000

    Create SumArray.java

---
+++ All programs/Chapter  6  Arrays and ArrayLists/SumArray.java
@@ -0,0 +1,15 @@
+// Fig. 6.5: SumArray.java
+// Computing the sum of the elements of an array.
+
+public class SumArray {
+	public static void main(String[] args) {
+		int[] array = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
+		int total = 0;
+		
+		// add each element's value to total
+		for (int counter = 0; counter < array.length; counter++)
+			total += array[counter];
+		
+		System.out.printf("Total of array elements: %d%n", total);
+	}
+} // end class SumArray
