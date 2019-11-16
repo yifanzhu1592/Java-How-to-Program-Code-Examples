a31efd747f45f5afa127529e88943f2349e48881
Yifan Zhu <zhuyi@tcd.ie>
2019-11-16 10:58:00 +0000

    Create DrawRainbowTest.java

---
+++ All programs/Chapter  6  Arrays and ArrayLists/DrawRainbowTest.java
@@ -0,0 +1,15 @@
+// Fig. 6.19: DrawRainbowTest.java
+// Test application to display a rainbow.
+import javax.swing.JFrame;
+
+public class DrawRainbowTest {
+	public static void main(String[] args) {
+		DrawRainbow panel = new DrawRainbow();
+		JFrame application = new JFrame();
+		
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		application.add(panel);
+		application.setSize(400, 250);
+		application.setVisible(true);
+	}
+} // end class DrawRainbowTest
