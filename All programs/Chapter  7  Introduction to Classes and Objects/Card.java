acc98afae9d90962b64af0a5c2f2a3cde99c2dd6
Yifan Zhu <zhuyi@tcd.ie>
2019-12-15 21:24:00 +0000

    Create Card.java

---
+++ All programs/Chapter  7  Introduction to Classes and Objects/Card.java
@@ -0,0 +1,18 @@
+// Fig. 7.11: Card.java
+// Card class represents a playing card.
+
+public class Card {
+	private final String face; // face of card ("Ace", "Deuce", ...)
+	private final String suit; // suit of card ("Hearts", "Diamonds", ...)
+	
+	// two-argument constructor initializes card's face and suit
+	public Card(String cardFace, String cardSuit) {
+		this.face = cardFace; // initialize face of card
+		this.suit = cardSuit; // initialize suit of card
+	}
+	
+	// return String representation of Card
+	public String toString() {
+		return face + " of " + suit;
+	}
+} // end class Card
