03ae4a82f064bfa14965f74169e896dd2b25894d
Yifan Zhu <zhuyi@tcd.ie>
2019-12-19 20:47:06 +0000

    Create MemberAccessTest.java

---
+++ All programs/Chapter  8  Classes and Objects: A Deeper Look/MemberAccessTest.java
@@ -0,0 +1,12 @@
+// Fig. 8.3: MemberAccessTest.java
+// Private members of class Time1 are not accessible.
+
+public class MemberAccessTest {
+	public static void main(String[] args) {
+		Time1 time = new Time1(); // create and initialize Time1 object
+		
+		time.hour = 7; // error: hour has private access in Time1
+		time.minute = 15; // error: minute has private access in Time1
+		time.second = 30; // error: second has private access in Time1
+	}
+} // end class MemberAccessTest
