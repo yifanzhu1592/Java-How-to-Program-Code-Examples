1a5a9eaf04c2075752db57c79502bc46e2495254
Yifan Zhu <zhuyi@tcd.ie>
2019-12-31 09:54:45 +0000

    Create ComboBoxTest.java

---
+++ All programs/Chapter 12  GUI Components: Part Ⅰ/ComboBoxTest.java
@@ -0,0 +1,12 @@
+// Fig. 12.22: ComboBoxTest.java
+// Testing ComboBoxFrame.
+import javax.swing.JFrame;
+
+public class ComboBoxTest {
+	public static void main(String[] args) {
+		ComboBoxFrame comboBoxFrame = new ComboBoxFrame();
+		comboBoxFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		comboBoxFrame.setSize(350, 150);
+		comboBoxFrame.setVisible(true);
+	}
+} // end class ComboBoxTest
