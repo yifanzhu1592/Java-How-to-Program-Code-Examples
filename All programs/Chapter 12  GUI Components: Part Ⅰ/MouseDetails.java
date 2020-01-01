ca611f417e1d0e680a3a5338e0c8b8bbb8b9992d
Yifan Zhu <zhuyi@tcd.ie>
2020-01-01 21:34:38 +0000

    Create MouseDetails.java

---
+++ All programs/Chapter 12  GUI Components: Part Ⅰ/MouseDetails.java
@@ -0,0 +1,12 @@
+// Fig. 12.32: MouseDetails.java
+// Testing MouseDetailsFrame.
+import javax.swing.JFrame;
+
+public class MouseDetails {
+	public static void main(String[] args) {
+		MouseDetailsFrame mouseDetailsFrame = new MouseDetailsFrame();
+		mouseDetailsFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mouseDetailsFrame.setSize(400, 150);
+		mouseDetailsFrame.setVisible(true);
+	}
+} // end class MouseDetails
