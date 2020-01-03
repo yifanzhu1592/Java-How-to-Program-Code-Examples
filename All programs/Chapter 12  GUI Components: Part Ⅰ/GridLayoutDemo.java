29de43b4f24ee0faf227191a284c197540df093e
Yifan Zhu <zhuyi@tcd.ie>
2020-01-03 16:12:32 +0000

    Create GridLayoutDemo.java

---
+++ All programs/Chapter 12  GUI Components: Part Ⅰ/GridLayoutDemo.java
@@ -0,0 +1,12 @@
+// Fig. 12.44: GridLayoutDemo.java
+// Testing GridLayoutFrame.
+import javax.swing.JFrame;
+
+public class GridLayoutDemo {
+	public static void main(String[] args) {
+		GridLayoutFrame gridLayoutFrame = new GridLayoutFrame();
+		gridLayoutFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gridLayoutFrame.setSize(300, 200);
+		gridLayoutFrame.setVisible(true);
+	}
+} // end class GridLayoutDemo
