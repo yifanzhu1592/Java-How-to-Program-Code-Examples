6ecb4c4a1f5a072199f4e31cd8023da9aa60fb66
Yifan Zhu <zhuyi@tcd.ie>
2020-01-03 10:49:18 +0000

    Create FlowLayoutDemo.java

---
+++ All programs/Chapter 12  GUI Components: Part Ⅰ/FlowLayoutDemo.java
@@ -0,0 +1,12 @@
+// Fig. 12.40: FlowLayoutDemo.java
+// Testing FlowLayoutFrame.
+import javax.swing.JFrame;
+
+public class FlowLayoutDemo {
+	public static void main(String[] args) {
+		FlowLayoutFrame flowLayoutFrame = new FlowLayoutFrame();
+		flowLayoutFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		flowLayoutFrame.setSize(300, 75);
+		flowLayoutFrame.setVisible(true);
+	}
+} // end class FlowLayoutDemo
