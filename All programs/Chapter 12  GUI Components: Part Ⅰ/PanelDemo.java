d8160d3243b8005943e853c6bb6c65ae36cda1be
Yifan Zhu <zhuyi@tcd.ie>
2020-01-04 10:26:55 +0000

    Create PanelDemo.java

---
+++ All programs/Chapter 12  GUI Components: Part Ⅰ/PanelDemo.java
@@ -0,0 +1,12 @@
+// Fig. 12.46: PanelDemo.java
+// Testing PanelFrame.
+import javax.swing.JFrame;
+
+public class PanelDemo extends JFrame {
+	public static void main(String[] args) {
+		PanelFrame panelFrame = new PanelFrame();
+		panelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panelFrame.setSize(450, 200);
+		panelFrame.setVisible(true);
+	}
+} // end class PanelDemo
