f9a402205fe9bd2dde0243d6596c1f1425fe05ed
Yifan Zhu <zhuyi@tcd.ie>
2020-01-04 15:35:24 +0000

    Create ShowColors2.java

---
+++ All programs/Chapter 12  GUI Components: Part Ⅰ/ShowColors2.java
@@ -0,0 +1,11 @@
+// Fig. 13.8: ShowColors2.java
+// Choosing colors with JColorChooser.
+import javax.swing.JFrame;
+
+public class ShowColors2 {
+	// execute application
+	public static void main(String[] args) {
+		ShowColors2JFrame application = new ShowColors2JFrame();
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+} // end class ShowColors2
