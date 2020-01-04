d3ee4da4e3ce9c1a534974787393b1c4a2769a04
Yifan Zhu <zhuyi@tcd.ie>
2020-01-04 14:10:04 +0000

    Create ColorJPanel.java

---
+++ All programs/Chapter 12  GUI Components: Part Ⅰ/ColorJPanel.java
@@ -0,0 +1,36 @@
+// Fig. 13.5: ColorJPanel.java
+// Changing drawing colors.
+import java.awt.Graphics;
+import java.awt.Color;
+import javax.swing.JPanel;
+
+public class ColorJPanel extends JPanel {
+	// draw rectangles and Strings in different colors
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		this.setBackground(Color.WHITE);
+		
+		// set new drawing color using integers
+		g.setColor(new Color(255, 0, 0));
+		g.fillRect(15, 25, 100, 20);
+		g.drawString("Current RGB: " + g.getColor(), 130, 40);
+		
+		// set new drawing color using floats
+		g.setColor(new Color(0.50f, 0.75f, 0.0f));
+		g.fillRect(15, 50, 100, 20);
+		g.drawString("Current RGB: " + g.getColor(), 130, 65);
+		
+		// set new drawing color using static Color objects
+		g.setColor(Color.BLUE);
+		g.fillRect(15, 75, 100, 20);
+		g.drawString("Current RGB: " + g.getColor(), 130, 90);
+		
+		// display individual RGB values
+		Color color = Color.MAGENTA;
+		g.setColor(color);
+		g.fillRect(15, 100, 100, 20);
+		g.drawString("RGB values: " + color.getRed() + ", " +
+				color.getGreen() + ", " + color.getBlue(), 130, 115);
+	}
+} // end class ColorJPanel
