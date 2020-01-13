abb7e88b2aa1af1af3fd0084423ae0b6fe52548e
Yifan Zhu <zhuyi@tcd.ie>
2020-01-13 00:04:57 +0000

    Create JFileChooserTest.java

---
+++ All programs/Chapter 15  Files, Streams and Object Serialization/JFileChooserTest.java
@@ -0,0 +1,13 @@
+// Fig. 15.13: JFileChooserTest.java
+// Tests class JFileChooserDemo.
+import java.io.IOException;
+import javax.swing.JFrame;
+
+public class JFileChooserTest {
+	public static void main(String[] args) throws IOException {
+		JFileChooserDemo application = new JFileChooserDemo();
+		application.setSize(400, 400);
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		application.setVisible(true);
+	}
+} // end class JFileChooserTest
