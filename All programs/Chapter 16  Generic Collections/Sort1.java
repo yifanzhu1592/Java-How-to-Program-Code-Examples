ded5877ca6425fabcbcaff0c87ca83ee38f6dc0b
Yifan Zhu <zhuyi@tcd.ie>
2020-01-14 10:55:05 +0000

    Create Sort1.java

---
+++ All programs/Chapter 16  Generic Collections/Sort1.java
@@ -0,0 +1,18 @@
+// Fig. 16.6: Sort1.java
+// Collections method sort.
+import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Sort1 {
+	public static void main(String[] args) {
+		String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+		
+		// Create and display a list containing the suits array elements
+		List<String> list = Arrays.asList(suits);
+		System.out.printf("Unsorted array elements: %s%n", list);
+		
+		Collections.sort(list); // sort ArrayList
+		System.out.printf("Sorted array elements: %s%n", list);
+	}
+} // end class Sort1
