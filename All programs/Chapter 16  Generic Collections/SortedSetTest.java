58a08e10cd3d5388ae0558fae613630ad06bfa8f
Yifan Zhu <zhuyi@tcd.ie>
2020-01-15 18:45:45 +0000

    Create SortedSetTest.java

---
+++ All programs/Chapter 16  Generic Collections/SortedSetTest.java
@@ -0,0 +1,37 @@
+// Fig. 16.17: SortedSetTest.java
+// Using SortedSets and TreeSets.
+import java.util.Arrays;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class SortedSetTest {
+	public static void main(String[] args) {
+		// create TreeSet from array colors
+		String[] colors = {"yellow", "green", "black", "tan", "grey",
+				"white", "orange", "red", "green"};
+		SortedSet<String> tree = new TreeSet<>(Arrays.asList(colors));
+		
+		System.out.print("sorted set: ");
+		printSet(tree);
+		
+		// get headSet based on "orange"
+		System.out.print("headSet (\"orange\"):  ");
+		printSet(tree.headSet("orange"));
+		
+		// get tailSet based on "orange"
+		System.out.print("tailSet (\"orange\"):  ");
+		printSet(tree.tailSet("orange"));
+		
+		// get first and last elements
+		System.out.printf("first: %s%n", tree.first());
+		System.out.printf("last : %s%n", tree.last());
+	}
+	
+	// output SortedSet using enhanced for statement
+	private static void printSet(SortedSet<String> set) {
+		for (String s : set)
+			System.out.printf("%s ", s);
+		
+		System.out.println();
+	}
+} // end class SortedSetTest
