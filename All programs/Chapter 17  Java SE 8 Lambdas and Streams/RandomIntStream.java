1def8af09241b0c41dc3cb65406ee304e10a8e81
Yifan Zhu <zhuyi@tcd.ie>
2020-01-19 16:43:46 +0000

    Create RandomIntStream.java

---
+++ All programs/Chapter 17  Java SE 8 Lambdas and Streams/RandomIntStream.java
@@ -0,0 +1,22 @@
+// Fig. 17.19: RandomIntStream.java
+// Rolling a die 6,000,000 times with streams
+import java.security.SecureRandom;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+
+public class RandomIntStream {
+	public static void main(String[] args) {
+		SecureRandom random = new SecureRandom();
+		
+		// roll a die 6,000,000 times and summarize the results
+		System.out.printf("%-6s%s%n", "Face", "Frequency");
+		random.ints(6_000_000, 1, 7)
+		      .boxed()
+		      .collect(Collectors.groupingBy(Function.identity(),
+		    		  Collectors.counting()))
+		      .forEach((face, frequency) ->
+		    		  System.out.printf("%-6d%d%n", face, frequency));
+	}
+} // end class RandomIntStream
