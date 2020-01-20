f9a1bdc0913dc741e115fdf33ba6b434d32144c7
Yifan Zhu <zhuyi@tcd.ie>
2020-01-20 06:42:25 +0000

    Create FactorialCalculator.java

---
+++ All programs/Chapter 18  Recursion/FactorialCalculator.java
@@ -0,0 +1,19 @@
+// Fig. 18.3: FactorialCalculator.java
+// Recursive factorial method.
+
+public class FactorialCalculator {
+	// recursive method factorial (assumes its parameter is >= 0)
+	public static long factorial(long number) {
+		if (number <= 1) // test for base case
+			return 1; // base cases: 0! = 1 and 1! = 1
+		else // recursion step
+			return number * factorial(number - 1);
+	}
+	
+	// output factorials for values 0-21
+	public static void main(String[] args) {
+		// calculate the factorials of 0 through 21
+		for (int counter = 0; counter <= 21; counter++)
+			System.out.printf("%d! = %d%n", counter, factorial(counter));
+	}
+} // end class FactorialCalculator
