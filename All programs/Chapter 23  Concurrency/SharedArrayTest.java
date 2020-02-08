01e24c3a671163768fd13d6586fda55c0a7a3dce
Yifan Zhu <zhuyi@tcd.ie>
2020-02-08 22:50:14 +0000

    Create SharedArrayTest.java

---
+++ All programs/Chapter 23  Concurrency/SharedArrayTest.java
@@ -0,0 +1,40 @@
+// Fig. 23.7: SharedArrayTest.java
+// Executing two Runnables to add elements to a shared SimpleArray.
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class SharedArrayTest {
+	public static void main(String[] args) {
+		// construct the shared object
+		SimpleArray sharedSimpleArray = new SimpleArray(6);
+		
+		// create two tasks to write to the shared SimpleArray
+		ArrayWriter writer1 = new ArrayWriter(1, sharedSimpleArray);
+		ArrayWriter writer2 = new ArrayWriter(11, sharedSimpleArray);
+		
+		// execute the tasks with an ExecutorService
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		executorService.execute(writer1);
+		executorService.execute(writer2);
+		
+		executorService.shutdown();
+		
+		try {
+			// wait 1 minute for both writers to finish executing
+			boolean tasksEnded =
+					executorService.awaitTermination(1, TimeUnit.MINUTES);
+			
+			if (tasksEnded) {
+				System.out.printf("%nContents of SimpleArray:%n");
+				System.out.println(sharedSimpleArray); // print contents
+			}
+			else
+				System.out.println(
+						"Timed out while waiting for tasks to finish.");
+		}
+		catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+	} // end main
+} // end class SharedArrayTest
