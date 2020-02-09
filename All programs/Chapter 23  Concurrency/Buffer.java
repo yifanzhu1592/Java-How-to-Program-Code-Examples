2dd7912d5e04d10f3d84d143841785ab2bf88cfc
Yifan Zhu <zhuyi@tcd.ie>
2020-02-09 18:13:47 +0000

    Update Buffer.java

--- All programs/Chapter 23  Concurrency/Buffer.java
+++ All programs/Chapter 23  Concurrency/Buffer.java
@@ -1,7 +1,7 @@
 // Fig. 23.9: Buffer.java
 // Buffer interface specifies methods called by Producer and Consumer.
 
-public class Buffer {
+public interface Buffer {
 	// place int value into Buffer
 	public void blockingPut(int value) throws InterruptedException;
 	
